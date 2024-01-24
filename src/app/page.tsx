0d4c896dea727eed9750bad914a8cc439076a35e
Noelia-F <noelia.fronton.g@gmail.com>
2024-01-24 22:21:15 +0100

    feat: login page skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,41 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import "./page.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="main">
+      <section className="section">
+          <h1 className="title">Where the dreams come true</h1>
+      </section>
+      <section className="section section--form">
+        <article className="form-wrapper">
+          <section className="form-wrapper__title-box">
+            <h2 className="form-wrapper__title">Login</h2>
+          </section>
+          <form>
+            <input type="text" placeholder="Userame..." />
+            <input type="text" placeholder="Password..." />
+            <label>
+              <input type="checkbox" id="cbox1" />
+              Remember me
+            </label>
+            <p>Forgot your password?</p>
+            <button type="submit">Login</button>
+          </form>
+          <section className="form-wrapper__login-app">
+            <p>Or login with...</p>
+            <ul>
+              <li>Google</li>
+              <li>Twitter</li>
+              <li>Facebook</li>
+            </ul>
+          </section>
+          <section className="form-wrapper__forgot">
+            <p>Don't you have an account?</p>
+            <a href="#">Register access</a>
+          </section>
+        </article>
+      </section>
     </main>
   );
 }
